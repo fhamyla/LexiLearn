b18094df240816e187cc4308575a32ff01dd0c15
fhamyla <fhamyla.devera@gmail.com>
2025-07-18 17:57:44 +0800

    added space bellow sign up button

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -217,6 +217,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 'auto',
+    marginBottom: 32, // 2rem in px
   },
   signUpText: {
     fontSize: 16,
